0cbea2758c7afcdb223ad5b629cd6cf904a8958c
3-Hz <edward.h.zhao@gmail.com>
2021-02-10 13:59:34 -0600

    init setup

---
+++ server/index.js
@@ -0,0 +1,25 @@
+//require('newrelic');
+const express = require('express');
+const cors = require('cors');
+const Model = require('./db/model.js');
+
+const M = new Model();
+M.connect();
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api/reviews/:productid', (req, res) => {
+  M.getReviews(req.params.productid)
+  .then(result => {
+    res.json(result.rows);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500);
+  });
+})
+
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
